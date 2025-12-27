f89b0b80d60ab6392d06f410435be412e4f32c52
riella <wooriel@gmail.com>
2025-12-28 04:52:07 +0900

    sdir name

--- util/pre_util.py
+++ util/pre_util.py
@@ -101,7 +101,7 @@ def read_seg_res_with_eseg_fname(dir_path, layer=0):
     """
     if layer == 1:
         dirnames = [d for d in os.listdir(dir_path)] # dir names - classs
-        sdirnames = sorted(dirnames, key=lambda d: int(d.split(".")[0].split("_")[0]))
+        sdirnames = sorted(dirnames, key=lambda d: int(d.split(".")[0].split("_")[0])) # sorted in int size
         face_labels = []
         eseg_dirs = []
         seseg_dirs = []

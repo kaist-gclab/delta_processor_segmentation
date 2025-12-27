9718dffd2f206d6a7193974188e5e4fc19d4140c
riella <wooriel@gmail.com>
2025-12-28 02:00:27 +0900

    layer0 inst

--- util/pre_util.py
+++ util/pre_util.py
@@ -53,7 +53,7 @@ def read_seg_res(dir_path, layer=0):
         seg_label_names (python list): doubly list of tags
     """
     if layer == 0:
-        filenames = [d for d in os.listdir(dir_path)]
+        filenames = [d for d in os.listdir(dir_path)] # directory names
         sfilenames = sorted(filenames, key=lambda d: int(d.split(".")[0].split("_")[0]))
         seg_files = ["{}.npz".format(f.split(".")[0]) for f in sfilenames]
         point_labels = []

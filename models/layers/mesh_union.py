7de8e92d0afccd38bc9eeaeb568a2a5e502e3e67
riella <wooriel@gmail.com>
2025-12-29 06:14:42 +0900

    padding dim

--- models/layers/mesh_union.py
+++ models/layers/mesh_union.py
@@ -49,7 +49,7 @@ class MeshUnion:
         """_summary_: reshape membership feature to match edge mask"""
         tensor_mask = torch.from_numpy(mask)
         self.groups = torch.clamp(self.groups[tensor_mask, :], 0, 1).transpose_(1, 0) # (e_new, n)
-        padding_a = features.shape[1] - self.groups.shape[0]
+        padding_a = features.shape[1] - self.groups.shape[0] # orig_edge - new_edge
         if padding_a > 0:
             padding_a = ConstantPad2d((0, 0, 0, padding_a), 0)
             self.groups = padding_a(self.groups)

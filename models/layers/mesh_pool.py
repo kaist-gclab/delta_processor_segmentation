13aa2b78011aea4e7bdf811a94d64562b0963d28
riella <wooriel@gmail.com>
2025-12-28 10:38:34 +0900

    remove comm

--- models/layers/mesh_pool.py
+++ models/layers/mesh_pool.py
@@ -44,8 +44,6 @@ class MeshPool(nn.Module):
     def __pool_main(self, mesh_index):
         mesh = self.__meshes[mesh_index]
         queue = self.__build_queue(self.__fe[mesh_index, :, :mesh.edges_count], mesh.edges_count)
-        # recycle = []
-        # last_queue_len = len(queue)
         last_count = mesh.edges_count + 1
         mask = np.ones(mesh.edges_count, dtype=np.bool)
         edge_groups = MeshUnion(mesh.edges_count, self.__fe.device)
--- models/layers/mesh_union.py
+++ models/layers/mesh_union.py
@@ -3,8 +3,7 @@ from torch.nn import ConstantPad2d
 
 
 class MeshUnion:
-    """_summary_: group of edge and its functions
-    """
+    """_summary_: group of edge and its functions"""
     def __init__(self, n, device=torch.device('cpu')):
         self.__size = n
         self.rebuild_features = self.rebuild_features_average

f6113c9f089b8044c8175cd33bfe9b53be4ab28d
riella <wooriel@gmail.com>
2025-12-30 03:57:54 +0900

    unpacked value desc

--- models/layers/mesh_pool.py
+++ models/layers/mesh_pool.py
@@ -134,7 +134,7 @@ class MeshPool(nn.Module):
         # side (int): side to collapse
         info = MeshPool.__get_face_info(mesh, edge_id, side) # get local face info
         key_a, key_b, side_a, side_b, _, other_side_b, _, other_keys_b = info # unpack into tuple
-        
+        # key_a: edge id to keep
         self.__redirect_edges(mesh, key_a, side_a - side_a % 2, other_keys_b[0], mesh.sides[key_b, other_side_b])
         self.__redirect_edges(mesh, key_a, side_a - side_a % 2 + 1, other_keys_b[1], mesh.sides[key_b, other_side_b + 1])
         MeshPool.__union_groups(mesh, edge_groups, key_b, key_a)

51ba37cfd531eed2db03f54a31f4946a3ccc739f
riella <wooriel@gmail.com>
2025-12-30 06:31:12 +0900

    backtraverse from edge a

--- models/layers/mesh_pool.py
+++ models/layers/mesh_pool.py
@@ -197,7 +197,7 @@ class MeshPool(nn.Module):
         mesh.gemm_edges[edge_a_key, side_a] = edge_b_key # edge_a's neighbor of slot a = edge_b
         mesh.gemm_edges[edge_b_key, side_b] = edge_a_key # edge_b's neighbor of slot b = edge_a
         mesh.sides[edge_a_key, side_a] = side_b # edge_b > edge_a = uses slot b
-        mesh.sides[edge_b_key, side_b] = side_a # edge
+        mesh.sides[edge_b_key, side_b] = side_a # edge_a > edge_b = uses slot a
 
     @staticmethod
     def __get_shared_items(list_a, list_b):

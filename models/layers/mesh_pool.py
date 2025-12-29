fd9e9efc35064d4f21c0a6c871bd6d5d526cb397
riella <wooriel@gmail.com>
2025-12-30 06:30:46 +0900

    update side (back traverse)

--- models/layers/mesh_pool.py
+++ models/layers/mesh_pool.py
@@ -196,8 +196,8 @@ class MeshPool(nn.Module):
         # edge keys: edge id | side: slot index
         mesh.gemm_edges[edge_a_key, side_a] = edge_b_key # edge_a's neighbor of slot a = edge_b
         mesh.gemm_edges[edge_b_key, side_b] = edge_a_key # edge_b's neighbor of slot b = edge_a
-        mesh.sides[edge_a_key, side_a] = side_b
-        mesh.sides[edge_b_key, side_b] = side_a
+        mesh.sides[edge_a_key, side_a] = side_b # edge_b > edge_a = uses slot b
+        mesh.sides[edge_b_key, side_b] = side_a # edge
 
     @staticmethod
     def __get_shared_items(list_a, list_b):

0766c6cfafadf06887142764abb43167357d5b69
riella <wooriel@gmail.com>
2025-12-30 06:27:04 +0900

    add argument spec

--- models/layers/mesh_pool.py
+++ models/layers/mesh_pool.py
@@ -193,6 +193,7 @@ class MeshPool(nn.Module):
     def __redirect_edges(mesh, edge_a_key, side_a, edge_b_key, side_b):
         """_summary_: rewire edge-neighborhood adjacency between two edges"""
         # link: (edge_a_key, side_a)  <-->  (edge_b_key, side_b)
+        # edge keys: edge id | side: slot index
         mesh.gemm_edges[edge_a_key, side_a] = edge_b_key # 
         mesh.gemm_edges[edge_b_key, side_b] = edge_a_key
         mesh.sides[edge_a_key, side_a] = side_b

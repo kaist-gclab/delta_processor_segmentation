67e693da51d1eaafbefb0f191ce2c52a4786c64d
riella <wooriel@gmail.com>
2025-12-30 02:29:26 +0900

    remove original edge

--- models/layers/mesh.py
+++ models/layers/mesh.py
@@ -220,7 +220,7 @@ class Mesh:
     def remove_group(self, index):
         """_summary_: remove edge group (after pooling)"""
         if self.history_data is not None:
-            self.history_data['edges_mask'][-1][self.history_data['current2old'][index]] = 0
+            self.history_data['edges_mask'][-1][self.history_data['current2old'][index]] = 0 # original edge = removed
             self.history_data['old2current'][self.history_data['current2old'][index]] = -1
             if self.export_folder:
                 self.history_data['collapses'].remove_group(self.history_data['current2old'][index])

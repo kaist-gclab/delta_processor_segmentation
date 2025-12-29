7a8b5c57bb95c1852d956b7627f3b36abf389d88
riella <wooriel@gmail.com>
2025-12-30 06:49:11 +0900

    list of lists to use the structure

--- models/layers/mesh_pool.py
+++ models/layers/mesh_pool.py
@@ -250,7 +250,7 @@ class MeshPool(nn.Module):
         if squared_magnitude.shape[-1] != 1:
             squared_magnitude = squared_magnitude.unsqueeze(-1) # (e_num, 1,)
         edge_ids = torch.arange(edges_count, device=squared_magnitude.device, dtype=torch.float32).unsqueeze(-1) # convert type, heapq > cast edge id back to int
-        heap = torch.cat((squared_magnitude, edge_ids), dim=-1).tolist()
+        heap = torch.cat((squared_magnitude, edge_ids), dim=-1).tolist() # list of lists (to use heapq structure)
         heapify(heap)
         return heap
 

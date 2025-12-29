99c324cc8d061677262d3bc7164a7e4bb464c232
riella <wooriel@gmail.com>
2025-12-29 20:59:00 +0900

    mask visited

--- models/layers/mesh.py
+++ models/layers/mesh.py
@@ -161,10 +161,10 @@ class Mesh:
                 continue # skip
             cycles.append([]) # append empty lst
             for i in range(3):
-                tmp_next_key = gemm[next_key, next_side]
+                tmp_next_key = gemm[next_key, next_side] # get neighbor
                 tmp_next_side = self.sides[next_key, next_side]
                 tmp_next_side = tmp_next_side + 1 - 2 * (tmp_next_side % 2)
-                gemm[next_key, next_side] = -1
+                gemm[next_key, next_side] = -1 # mask visited
                 gemm[next_key, next_side + 1 - 2 * (next_side % 2)] = -1
                 next_key = tmp_next_key
                 next_side = tmp_next_side

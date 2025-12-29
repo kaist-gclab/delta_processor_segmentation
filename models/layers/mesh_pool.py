bf662cdad0672283f7fe2d9e8c9dfd98c3788a31
riella <wooriel@gmail.com>
2025-12-30 03:52:42 +0900

    pool side desc

--- models/layers/mesh_pool.py
+++ models/layers/mesh_pool.py
@@ -126,6 +126,7 @@ class MeshPool(nn.Module):
         return len(shared) == 2
 
     def __pool_side(self, mesh, edge_id, mask, edge_groups, side):
+        """_summary_: collapse one face side adjacent to edge_id"""
         info = MeshPool.__get_face_info(mesh, edge_id, side)
         key_a, key_b, side_a, side_b, _, other_side_b, _, other_keys_b = info
         self.__redirect_edges(mesh, key_a, side_a - side_a % 2, other_keys_b[0], mesh.sides[key_b, other_side_b])
